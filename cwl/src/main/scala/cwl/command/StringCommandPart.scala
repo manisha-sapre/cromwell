60a3382cd8bf9b7cab6dbec40609db444f3cc1a0
Miguel Covarrubias <mcovarr@broadinstitute.org>
2018-01-12 18:07:37 -0500

    hardcode shellquoting on, fix a conformance test

--- cwl/src/main/scala/cwl/command/StringCommandPart.scala
+++ cwl/src/main/scala/cwl/command/StringCommandPart.scala
@@ -7,10 +7,21 @@ import wom.callable.RuntimeEnvironment
 import wom.expression.IoFunctionSet
 import wom.graph.LocalName
 import wom.values.WomValue
+import StringCommandPart._
 
 case class StringCommandPart(literal: String) extends CommandPart {
   override def instantiate(inputsMap: Map[LocalName, WomValue],
                            functions: IoFunctionSet,
                            valueMapper: (WomValue) => WomValue,
-                           runtimeEnvironment: RuntimeEnvironment): ErrorOr[List[InstantiatedCommand]] = List(InstantiatedCommand(literal)).validNel
+                           runtimeEnvironment: RuntimeEnvironment): ErrorOr[List[InstantiatedCommand]] =
+    // TODO CWL shellquotes by default, but this shellquotes everything.
+    List(InstantiatedCommand(literal.shellQuote)).validNel
+}
+
+object StringCommandPart {
+
+  implicit class ShellQuoteHelper(val s: String) extends AnyVal {
+    // Escape any double quote characters in the string and surround with double quotes.
+    def shellQuote: String = '"' + s.replaceAll("\"", "\\\"") + '"'
+  }
 }
